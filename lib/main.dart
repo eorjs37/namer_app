0c31d205842386c19df7ace8b9413554ed0c5c75
macdaegeon <chleorjs37@gmail.com>
2025-06-29 21:33:40 +0900

    fluuter app

--- lib/main.dart
+++ lib/main.dart
@@ -27,23 +27,34 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
+  // 1. 위젯의 상황이 변경될 때마다 자동으로 호출되느 build메소드를 정의해야함
   Widget build(BuildContext context) {
+    // 2. MyHomePage는 watch 메서드를 사용하여 앱의 현재 상태에 관한 변경사항을 추적합니다.
     var appState = context.watch<MyAppState>();
 
+    // 3.모든 build 메서드는 위젯 또는 중첩된 위젯 트리(좀 더 일반적임)를 반환해야 합니다. 여기서 최상위 위젯은 Scaffold입니다
     return Scaffold(
+      // 4. Column은 Flutter에서 가장 기본적인 레이아웃 위젯 중 하나입니다. Column은 위에서 아래로
       body: Column(
         children: [
+          // 5.첫 번째 단계에서 이 Text 위젯을 변경했습니다.
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          // 6. 이 두 번째 Text 위젯은 appState를 사용하고 해당 클래스의 유일한 멤버인 current(즉, WordPair)에 액세스합니다.
+          Text(appState.current.asUpperCase),
 
           // ↓ Add this.
           ElevatedButton(
               onPressed: () {
-                print('button pressed!');
+                appState.getNext();
               },
               child: Text('Next'))
         ],
